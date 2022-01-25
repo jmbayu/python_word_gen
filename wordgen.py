3a0d2ed5e1148cb2649e9e9c843cb3ec490bca67
Mbayu, John (415) <john.mbayu@daimler.com>
2022-01-26 00:01:36 +0100

    linting, add vscode stuff

--- wordgen.py
+++ wordgen.py
@@ -1,17 +1,23 @@
-# use all the letters in "herecat" to generate as many 3 lettered english words as possible
+""" use all the letters in "herecat" to generate as many 3 lettered english words as possible
+"""
 from itertools import permutations
 import enchant
-# use the spell checking dictionary with british english 
-dictionary = enchant.Dict("en_GB")
-olus_input = "herecat"
-minlen = 3
 
-output_set=set()
-letters=[x.lower() for x in olus_input]
-for n in range(len(olu_input)):
-  for y in list(permutations(letters, n)):
-    z="".join(y)
-    if len(z)>minlen and dictionary.check(z):
-      output_set.add(z)
-print( output_set )
-print(len(output_set))
+def main():
+    '''
+    Use the spell checking dictionary with british english
+    '''
+    use_dictionary = enchant.Dict("en_GB")
+    tibi_olus_ipnut = "herecat"
+    minimum_length = 3
+    output_set = set()
+    letters = [x.lower() for x in tibi_olus_ipnut]
+    for counter in range(len(tibi_olus_ipnut)):
+        for letter in list(permutations(letters, counter)):
+            new_word = "".join(letter)
+            if len(new_word) > minimum_length and use_dictionary.check(new_word):
+                output_set.add(new_word)
+    print(output_set)
+    print(len(output_set))
+
+main()
